fd48a7f6fb6bbdc605ba1649ddf4989df5247dd0
hydrotolerance <julzhafiz@gmail.com>
2024-04-04 21:45:42 +0800

    (Fix) Adding the  vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  // Other routes...
+  {
+    path: '/logout',
+    name: 'logout',
+    component: LogoutComponent
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
